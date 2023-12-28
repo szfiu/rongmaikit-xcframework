81f143a88cfecca7fea2e1afcbde37cfdfee8b57
itonyyo <swzyocowboy@icloud.com>
2023-12-28 11:14:02 +0800

    feat: 更新包

---
+++ xc/RongmaiKit.xcframework/ios-arm64/RongmaiKit.framework/Headers/RongmaiKit.h
@@ -0,0 +1,18 @@
+//
+//  RongmaiKit.h
+//  RongmaiKit
+//
+//  Created by iyowei on 2023/11/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RongmaiKit.
+FOUNDATION_EXPORT double RongmaiKitVersionNumber;
+
+//! Project version string for RongmaiKit.
+FOUNDATION_EXPORT const unsigned char RongmaiKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RongmaiKit/PublicHeader.h>
+
+
